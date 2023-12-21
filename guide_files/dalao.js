30c318df48ce883a4a4baabbd9ac9e616a0ceaf8
cvabm <lonva99@gmail.com>
2023-12-21 11:30:27 +0800

    commit message

---
+++ guide_files/dalao.js
@@ -0,0 +1,124 @@
+var dh_data = [
+  {
+    name: "资讯",
+    item: [
+      { name: "今日热榜", url: "https://tophub.today/" },
+      { name: "阮一峰", url: "http://www.ruanyifeng.com/blog" },
+      { name: "不死鸟", url: "https://iui.su" },
+      { name: "玩安卓", url: "https://www.wanandroid.com/" },
+      {
+        name: "GitHub排行",
+        url: "https://github.com/kon9chunkit/GitHub-Chinese-Top-Charts",
+      },
+    ],
+  },
+  {name:"命令速查",item:[
+    {name:"中文",url:"https://wangchujiang.com/reference/index.html"},
+    {name:"英文",url:"https://quickref.me/"},
+  ]},
+  {
+    name: "chatgpt镜像",
+    item: [
+      { name: "官方", url: "https://chat.openai.com/" },
+      { name: "ai55.cc", url: "https://cc.ai55.cc/" },
+      { name: "freegpt.cc", url: "https://freegpt.cc/" },
+      {
+        name: "carrot",
+        url: "https://github.com/xx025/carrot",
+      },
+      { name: "增强chrome插件", url: "https://greasyfork.org/zh-CN/scripts/462804-keepchatgpt" },
+    ],
+  },
+
+  {
+    name: "文件/文本分享工具",
+    item: [
+      { name: "netcut", url: "http://netcut.cn/" },
+      { name: "cmd.im", url: "https://cmd.im/" },
+      { name: "pasteme", url: "https://pasteme.cn/" },
+      { name: "anyknew", url: "https://cp.anyknew.com/" },
+      { name: "ko0", url: "https://ko0.com/" },
+      { name: "deershare", url: "https://deershare.com/send" },
+      { name: "Witeboard", url: "https://witeboard.com/" },
+    ],
+  },
+  {
+    name: "搜索",
+    item: [
+      { name: "startpage", url: "https://startpage.com/" },
+      { name: "Shodan", url: "https://www.shodan.io/" },
+      { name: "Fofa", url: "https://fofa.so/" },
+    ],
+  },
+  {
+    name: "代码搜索",
+    item: [
+      { name: "grep.app", url: "https://grep.app/" },
+      { name: "aixcoder", url: "https://codesearch.aixcoder.com/#/" },
+      { name: "Tabnine", url: "https://www.tabnine.com/code" },
+      { name: "searchcode", url: "https://searchcode.com/" },
+      { name: "cs.android.com", url: "https://cs.android.com/" },
+    ],
+  },
+  {
+    name: "API",
+    item: [
+      { name: "DevDocs", url: "https://devdocs.io/" },
+      { name: "JS.coach", url: "https://js.coach/" },
+      { name: "DevHints", url: "https://devhints.io/" },
+    ],
+  },
+  {
+    name: "教程",
+    item: [
+      { name: "wangdoc", url: "https://wangdoc.com" },
+      {
+        name: "Java面试题",
+        url: "http://www.jishuchi.com/read/java-interview/3346",
+      },
+      { name: "菜鸟教程", url: "https://m.runoob.com/" },
+      { name: "廖雪峰", url: "https://www.liaoxuefeng.com/" },
+      { name: "黑马视频", url: "http://yun.itheima.com/index/index.html" },
+      { name: "pdai.tech", url: "https://www.pdai.tech/md/interview/x-interview.html" },
+    ],
+  },
+  {
+    name: "其他",
+    item: [
+      { name: "草料二维码", url: "https://cli.im" },
+      { name: "接码导航", url: "https://jiema.pub" },
+      { name: "sms接码", url: "https://sms-activate.org" },
+      { name: "supercloudsms", url: "https://www.supercloudsms.com/" },
+      { name: "录屏1", url: "https://github.com/NickeManarin/ScreenToGif" },
+      { name: "录屏2", url: "https://www.screentogif.com/?l=zh_cn" },
+      { name: "录屏3", url: "https://www.cockos.com/licecap" },
+      { name: "看报", url: "https://reader.jojokanbao.cn" },
+      {name:"archive",url:"https://archive.org"},
+    ],
+  },
+  {
+    name: "看电影",
+    item: [
+      { name: "小纸条", url: "https://ali.gitcafe.ink/" },
+      { name: "硬核", url: "https://yingheapp.com/" },
+      { name: "茶杯虎", url: "https://725998.com/" },
+{ name: "茶杯狐", url: "https://cupfox.app" },
+{ name: "搜片", url: "https://soupian.one/" },
+{ name: "聚合", url: "https://www.juheyingshi.com" },
+    ],
+  },
+{
+  name:"图标素材",
+  item:[
+    {name:"yesicon",url:"https://yesicon.app/"},
+    {name:"flaticon",url:"https://www.flaticon.com/"},
+    {name:"iconfont",url:"https://www.iconfont.cn/"},
+     {name:"kitchen",url:"https://icon.kitchen"},
+     {name:"oceanengine",url:"https://iconpark.oceanengine.com"},
+     {name:"flaticon",url:"https://www.flaticon.com"},
+     {name:"thenounproject",url:"https://thenounproject.com"},
+    {name:"pixabay",url:"https://pixabay.com/"},
+    {name:"iconfinder",url:"https://iconfinder.com"},
+  ]
+}
+];
